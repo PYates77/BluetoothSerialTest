a5ba538a31f05305cefd51a2e1cb4781a186a4ec
Paul Yates <paulyates@Pauls-MBP.attlocal.net>
2019-08-23 18:17:06 -0400

    initial commit

---
+++ app/src/main/java/com/example/bluetoothserialtest/MainActivity.java
@@ -0,0 +1,293 @@
+package com.example.bluetoothserialtest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Button;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends Activity {
+    TextView myLabel;
+    EditText myTextbox;
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+
+    private LineChart mChart;
+    private Thread thread;
+
+    private int raw_data;
+    private final float SCREEN_HEIGHT = 6000000f;
+    private float graph_scalar = 10f;
+    private int graph_offset = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button openButton = (Button) findViewById(R.id.open);
+        Button sendButton = (Button) findViewById(R.id.send);
+        Button closeButton = (Button) findViewById(R.id.close);
+        Button tareButton = (Button) findViewById(R.id.tare);
+        Button calibrateButton = (Button) findViewById(R.id.calibrate);
+        myLabel = (TextView) findViewById(R.id.label);
+        myTextbox = (EditText) findViewById(R.id.entry);
+
+        mChart = (LineChart) findViewById(R.id.chart1);
+
+        // enable description text
+        mChart.getDescription().setEnabled(true);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        // set an alternative background color
+        mChart.setBackgroundColor(Color.WHITE);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add empty data
+        mChart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMaximum(6000000f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        mChart.getAxisLeft().setDrawGridLines(false);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
+
+        //Open Button
+        openButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    findBT();
+                    openBT();
+                } catch (IOException ex) {
+                }
+            }
+        });
+
+        //Send Button
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    sendData();
+                } catch (IOException ex) {
+                }
+            }
+        });
+
+        //Close button
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    closeBT();
+                } catch (IOException ex) {
+                }
+            }
+        });
+
+        //Tare button
+        tareButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // set such that the current raw_data value is at 0.1 screen height
+                // solve 0.1 * SCREEN_HEIGHT = graph_scalar * raw_data - graph_offset
+                graph_offset = (int) (graph_scalar * raw_data - 0.1 * SCREEN_HEIGHT);
+            }
+        });
+
+        //Calibrate button
+        calibrateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // set such that the current raw_data value takes up 0.4 times the screen
+                // solving equation 0.3 * SCREEN_HEIGHT = graph_scalar * raw_data - graph_offset
+                graph_scalar = (0.3f * SCREEN_HEIGHT + graph_offset) / raw_data;
+            }
+        });
+    }
+
+    void findBT() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            myLabel.setText("No bluetooth adapter available");
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                if (device.getName().equals("Load Cell")) {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+        myLabel.setText("Bluetooth Device Found");
+    }
+
+    void openBT() throws IOException {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+        beginListenForData();
+
+        myLabel.setText("Bluetooth Opened");
+    }
+
+    private LineDataSet createSet() {
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3f);
+        set.setColor(Color.MAGENTA);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
+    void beginListenForData() {
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable() {
+            public void run() {
+                while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+                    try {
+                        int bytesAvailable = mmInputStream.available();
+                        if (bytesAvailable > 0) {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for (int i = 0; i < bytesAvailable; i++) {
+                                byte b = packetBytes[i];
+                                if (b == delimiter) {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    raw_data = Integer.parseInt(data.trim());
+                                    final int data_i = (int) (graph_scalar * raw_data) - graph_offset;
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable() {
+                                        public void run() {
+                                            String text = data + "(" + data_i + ")";
+                                            myLabel.setText(text);
+
+                                            LineData ldata = mChart.getData();
+                                            if (ldata != null) {
+                                                ILineDataSet set = ldata.getDataSetByIndex(0);
+
+                                                if (set == null) {
+                                                    set = createSet();
+                                                    ldata.addDataSet(set);
+                                                }
+
+                                                ldata.addEntry(new Entry(set.getEntryCount(), data_i), 0);
+                                                ldata.notifyDataChanged();
+
+                                                mChart.notifyDataSetChanged();
+                                                mChart.setVisibleXRangeMaximum(50);
+                                                //mChart.setMaxVisibleValueCount(150);
+                                                mChart.moveViewToX(ldata.getEntryCount());
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    } catch (IOException ex) {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    void sendData() throws IOException {
+        String msg = myTextbox.getText().toString();
+        msg += "\n";
+        mmOutputStream.write(msg.getBytes());
+        myLabel.setText("Data Sent");
+    }
+
+    void closeBT() throws IOException {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+        myLabel.setText("Bluetooth Closed");
+    }
+}
